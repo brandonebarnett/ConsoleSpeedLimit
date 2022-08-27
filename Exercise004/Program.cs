ed3cbd71ffffbaa58ab7d9fcd1d91543e7f1e55c
Brandon <brandon.ebarnett@gmail.com>
2022-08-27 10:11:51 -0400

    Add project files.

---
+++ Exercise004/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Write a program that asks the user to enter the speed limit.
+ * Once the speed limit is set, the program should ask the speed of the vehicle.
+   If the speed of the vehicle is below or equal to the speed limit, the program
+   should display 'Ok'.  
+   If the speed of the vehicle is above the speed limit, the program should
+   calculate the number of demerit points.
+   - For every 5mph above the speed limit => 1 demerit point.
+   - If demerit points > 12 => License is suspended.*/
+
+Console.Write("What is the speed limit? ");
+byte speedLimit = Convert.ToByte(Console.ReadLine());
+
+Console.Write("How fast is the vehicle going? ");
+byte speedOfVehicle = Convert.ToByte(Console.ReadLine());
+
+CalculateDemeritPoints(speedLimit, speedOfVehicle);
+
+
+void CalculateDemeritPoints(byte speedLimit, byte speedOfVehicle)
+{
+    const byte mphPerDemeritPoint = 5;
+    var demeritPoints = (speedOfVehicle - speedLimit) / mphPerDemeritPoint;
+
+    if (speedOfVehicle <= speedLimit)
+        Console.WriteLine("Ok");
+    else
+    {
+        if (demeritPoints > 12)
+            Console.WriteLine("License Suspended.");
+        else
+            Console.WriteLine("Demerit Point: " + demeritPoints);
+    }
+}
